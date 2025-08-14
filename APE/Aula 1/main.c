87f9e0f24c7d4c22f77b4a39afa9438ae89f0267
Matheus S. Silva <matheussouza.silva@a.ucb.br>
2025-08-14 10:48:15 -0300

    Add files via upload

---
+++ APE/Aula 1/main.c
@@ -0,0 +1,14 @@
+/* 
+Aula01 de Algoritmo e Progamação Estruturada
+Objetivo: Escrever "Hello World!" em printf
+Entrada: N/A
+Saída: Hello World!
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello\tWorld!\n");
+	printf("Hello\tWorld!");
+	return 0;
+}
