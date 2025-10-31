8a92b0ce9ea8a112c8edef30a1cfc4fc70b4516c
Solux S. <matheussouza.silva@a.ucb.br>
2025-10-31 00:35:27 -0300

    structs2

--- atividades/structs2/main.c
+++ atividades/structs2/main.c
@@ -75,57 +75,6 @@ int main() {
 		}
 	}
 	
-	/*
-	Elabore um programa com base nas structs acima, receba o cadastro de 50 alunos e apresente ao final a maior idade identificada, e os nomes que tenham a respectiva idade.
-	*/
-	
-	char nome_completo[30];
-	char nome[30];
-	char sobrenome[30];
-	int i = 0, primeiro_espaco = 0, ultimo_espaco = 0, cont = 0;
-	
-	printf("--------------------------------\n");
-	printf("      Don't Worry Airlines      \n");
-	printf("--------------------------------\n");
-	
-	printf("Insira o seu Nome Completo: ");
-	scanf("%[^\n]s",&nome_completo);
-	
-	for(i = 0; i < strlen(nome_completo); i++){
-		if(nome_completo[i] == ' ' && cont == 0){
-			primeiro_espaco = i;
-			cont++;
-		}
-		
-		if(nome_completo[i] == ' '){
-			ultimo_espaco = i + 1;
-		}
-	}
-	
-	i = 0;
-	for(ultimo_espaco; ultimo_espaco < strlen(nome_completo); ultimo_espaco++){
-		sobrenome[i] = nome_completo[ultimo_espaco];
-		i++;
-	}
-	
-	for(i = 0; i < strlen(sobrenome); i++){
-		sobrenome[i] = toupper(sobrenome[i]);
-	}
-	
-	for(i = 0; i < primeiro_espaco; i++){
-		nome[i] = nome_completo[i];
-	}
-	
-	nome[0] = toupper(nome[0]);
-	for(i = 1; i < strlen(nome); i++){
-		nome[i] = tolower(nome[i]);
-	}
-	
-	printf("+-------------------------+\n");
-	printf("|       %s/%s             |\n",sobrenome,nome);
-	printf("|       Boa Viagem!       |\n");
-	printf("+-------------------------+");
-	
 	return 0;
 }
 
